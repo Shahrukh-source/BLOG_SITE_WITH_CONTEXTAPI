cd19ad40b5f8494ada17ff5d6c30277a2ef6ac5f
Shahrukh-source <sk1109697@gmail.com>
2023-11-28 22:25:39 -0800

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../App.css"
+const Header = () => {
+  return (
+    <div className='w-full border shadow-md py-4 fixed top-0 bg-white '>
+      <header className='text-center' >
+        <h1 className='color-#ff0000 text-3xl font-bold uppercase' >Codehelp Blogs</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
